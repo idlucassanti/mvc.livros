968506e68afeadc72ed55c152bb6a43844fb75bb
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2022-05-09 21:37:53 -0300

    [01] Criação projeto ASP.NET Core MVC

---
+++ src/Santi.Livros.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Santi.Livros.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
